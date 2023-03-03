e7d541a151fdd690103731d1635169cab57221f8
RuyHa <code.ruyha@gmail.com>
2023-03-03 20:20:27 +0900

    [feat] 일반적인 방식의 Api 값이 아닌 다른 값이 나올 때 랜덤 값을 요청하여 대체

--- Universe Today/Universe Today/Service/ApodService.swift
+++ Universe Today/Universe Today/Service/ApodService.swift
@@ -30,6 +30,8 @@ class ApodService {
             switch response.result {
             case .failure(let err) :
                 NSLog("setAPI/setApod Err : \(err)")
+                //하단주석 1참고
+                self.setRandomApod()
             case .success(let result) :
                 self.currentApodModel.accept(result)
             }
@@ -44,6 +46,8 @@ class ApodService {
             switch response.result {
             case .failure(let err) :
                 NSLog("setAPI/setRandomApod Err : \(err)")
+                //하단주석 1참고
+                self.setRandomApod()
             case .success(let result) :
                 guard let randomApod = result.first else {
                     NSLog("Error ApodService/setRandomApod/nil ")
@@ -79,3 +83,10 @@ class ApodService {
     
 }
 
+
+/*
+ MARK: 하단주석 1
+    받아온 값에 문제가 있을 경우 랜덤 이미지를 값을 출력시킴
+    해당 문제를 완벽히 처리하고 싶으면 api가 뿌리는 모든 결과값을 가지고 처리를 해야 하는데
+    문서에 나오지 않아서 확인하기가 어렵다.
+ */
